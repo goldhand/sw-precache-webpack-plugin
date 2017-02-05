f26f3a7e9db5cab9d2f67032be372084e3a0ce61
Will Farley <wfarley@amazon.com>
2017-02-04 16:40:41 -0800

    Refactor merge behavior
    Add unit tests for merge behavior
    
    Update readme with merge behavior

--- src/index.js
+++ src/index.js
@@ -12,68 +12,92 @@ const
   DEFAULT_WORKER_FILENAME = 'service-worker.js',
   DEFAULT_OUTPUT_PATH = '',
   DEFAULT_PUBLIC_PATH = '',
-  DEFAULT_IMPORT_SCRIPTS = [];
+  DEFAULT_IMPORT_SCRIPTS = [],
+  DEFAULT_IGNORE_PATTERNS = [];
 
 const DEFAULT_OPTIONS = {
   cacheId: DEFAULT_CACHE_ID,
   filename: DEFAULT_WORKER_FILENAME,
+  importScripts: DEFAULT_IMPORT_SCRIPTS,
+  staticFileGlobsIgnorePatterns: DEFAULT_IGNORE_PATTERNS,
   forceDelete: false,
   mergeStaticsConfig: false,
   minify: false,
 };
 
-/**
- * SWPrecacheWebpackPlugin - A wrapper for sw-precache to use with webpack
- * @param {object} options - All parameters should be passed as a single options object
- *
- * // sw-precache options:
- * @param {string} [options.cacheId]
- * @param {string} [options.directoryIndex]
- * @param {object|array} [options.dynamicUrlToDependencies]
- * @param {boolean} [options.handleFetch]
- * @param {array} [options.ignoreUrlParametersMatching]
- * @param {array} [options.importScripts]
- * @param {function} [options.logger]
- * @param {number} [options.maximumFileSizeToCacheInBytes]
- * @param {array} [options.navigateFallbackWhitelist]
- * @param {string} [options.replacePrefix]
- * @param {array} [options.runtimeCaching]
- * @param {array} [options.staticFileGlobs]
- * @param {string} [options.stripPrefix]
- * @param {string} [options.stripPrefixMulti]
- * @param {string} [options.templateFilePath]
- * @param {boolean} [options.verbose]
- *
- * // plugin options:
- * @param {string} [options.filename] - Service worker filename, default is 'service-worker.js'
- * @param {string} [options.filepath] - Service worker path and name, default is to use webpack.output.path + options.filename
- * @param {RegExp} [options.staticFileGlobsIgnorePatterns[]] - Define an optional array of regex patterns to filter out of staticFileGlobs
- * @param {boolean} [options.forceDelete=false] - Pass force option to del
- * @param {boolean} [options.mergeStaticsConfig=false] - Merge provided staticFileGlobs and stripPrefix(Multi) with webpack's config, rather than having those take precedence
- * @param {boolean} [options.minify=false] - Minify the generated Service worker file using UglifyJS
- */
+
 class SWPrecacheWebpackPlugin {
 
+  /**
+   * SWPrecacheWebpackPlugin - A wrapper for sw-precache to use with webpack
+   * @constructor
+   * @param {object} options - All parameters should be passed as a single options object
+   *
+   * // sw-precache options:
+   * @param {string} [options.cacheId]
+   * @param {string} [options.directoryIndex]
+   * @param {object|array} [options.dynamicUrlToDependencies]
+   * @param {boolean} [options.handleFetch]
+   * @param {array} [options.ignoreUrlParametersMatching]
+   * @param {array} [options.importScripts]
+   * @param {function} [options.logger]
+   * @param {number} [options.maximumFileSizeToCacheInBytes]
+   * @param {array} [options.navigateFallbackWhitelist]
+   * @param {string} [options.replacePrefix]
+   * @param {array} [options.runtimeCaching]
+   * @param {array} [options.staticFileGlobs]
+   * @param {string} [options.stripPrefix]
+   * @param {string} [options.stripPrefixMulti]
+   * @param {string} [options.templateFilePath]
+   * @param {boolean} [options.verbose]
+   *
+   * // plugin options:
+   * @param {string} [options.filename] - Service worker filename, default is 'service-worker.js'
+   * @param {string} [options.filepath] - Service worker path and name, default is to use webpack.output.path + options.filename
+   * @param {RegExp} [options.staticFileGlobsIgnorePatterns[]] - Define an optional array of regex patterns to filter out of staticFileGlobs
+   * @param {boolean} [options.forceDelete=false] - Pass force option to del
+   * @param {boolean} [options.mergeStaticsConfig=false] - Merge provided staticFileGlobs and stripPrefix(Multi) with webpack's config, rather than having those take precedence
+   * @param {boolean} [options.minify=false] - Minify the generated Service worker file using UglifyJS
+   */
   constructor(options) {
+    // generated configuration options
+    this.config = {};
+    // configuration options passed by user
     this.options = {
       ...DEFAULT_OPTIONS,
       ...options,
     };
+    // generated configuration that will override user options
     this.overrides = {};
+  }
+
+  /**
+   * @returns {object} - plugin configuration
+   */
+  get workerOptions() {
+    return {
+      ...this.config,
+      ...this.options,
+      ...this.overrides,
+    };
   }
 
   apply(compiler) {
 
     compiler.plugin('after-emit', (compilation, callback) => {
 
+      // get the defaults from options
+      const {
+        importScripts,
+        staticFileGlobsIgnorePatterns,
+        mergeStaticsConfig,
+      } = this.options;
+
       // get the output path specified in webpack config
       const outputPath = compiler.options.output.path || DEFAULT_OUTPUT_PATH;
 
       // get the public path specified in webpack config
       const {publicPath = DEFAULT_PUBLIC_PATH} = compiler.options.output;
-
-      // get the importScripts value specified in the sw-precache config
-      const {importScripts = DEFAULT_IMPORT_SCRIPTS} = this.options;
 
       if (this.options.filepath) {
         // warn about changing filepath
@@ -85,29 +109,25 @@ class SWPrecacheWebpackPlugin {
         .keys(compilation.assets)
         .map(f => path.join(outputPath, f));
 
-      const ignorePatterns = this.options.staticFileGlobsIgnorePatterns || [];
-
-      // merge assetGlobs with provided staticFileGlobs and filter using ignorePatterns
+      // merge assetGlobs with provided staticFileGlobs and filter using staticFileGlobsIgnorePatterns
       const staticFileGlobs = assetGlobs.concat(this.options.staticFileGlobs || []).filter(text =>
-        (!ignorePatterns.some((regex) => regex.test(text)))
+        (!staticFileGlobsIgnorePatterns.some((regex) => regex.test(text)))
       );
 
-      const config = {
-        staticFileGlobs,
-        // use provided stripPrefixMulti if there is one, then work from there
-        stripPrefixMulti: this.options.mergeStaticsConfig ? {...this.options.stripPrefixMulti} : {},
-        verbose: true,
+      const stripPrefixMulti = {
+        ...this.options.stripPrefixMulti,
       };
-
-      if (this.options.mergeStaticsConfig && this.options.stripPrefix) {
-        // add stripPrefix to stripPrefixMulti and delete it so we make sure only stripPrefixMulti is used
-        config.stripPrefixMulti[this.options.stripPrefix] = this.options.replacePrefix || '';
-      }
 
       if (outputPath) {
         // strip the webpack config's output.path
-        config.stripPrefixMulti[`${outputPath}${path.sep}`] = publicPath || '';
+        stripPrefixMulti[`${outputPath}${path.sep}`] = publicPath;
       }
+
+      this.config = {
+        ...this.config,
+        staticFileGlobs,
+        stripPrefixMulti,
+      };
 
       if (importScripts) {
         this.overrides.importScripts = importScripts
@@ -115,33 +135,30 @@ class SWPrecacheWebpackPlugin {
           .map(f => url.resolve(publicPath, f));  // add publicPath to importScripts
       }
 
+      if (mergeStaticsConfig) {
+        // merge generated and user provided options
+        this.overrides = {
+          ...this.overrides,
+          staticFileGlobs,
+          stripPrefixMulti,
+        };
+      }
+
       const done = () => callback();
       const error = (err) => callback(err);
 
-      this.writeServiceWorker(compiler, config).then(done, error);
+      this.writeServiceWorker(compiler).then(done, error);
     });
   }
 
-  writeServiceWorker(compiler, config) {
+  writeServiceWorker(compiler) {
     const
       fileDir = compiler.options.output.path || DEFAULT_OUTPUT_PATH,
       // default to options.filepath for writing service worker location
-      {filepath = path.join(fileDir, this.options.filename)} = this.options,
-      workerOptions = {
-        ...config,
-        ...this.options,
-        ...this.overrides,
-      };
-
-    if (this.options.mergeStaticsConfig) {
-      workerOptions.staticFileGlobs = config.staticFileGlobs;
-      workerOptions.stripPrefixMulti = config.stripPrefixMulti;
-      delete workerOptions.stripPrefix;
-      delete workerOptions.replacePrefix;
-    }
+      {filepath = path.join(fileDir, this.options.filename)} = this.options;
 
     return del(filepath, {force: this.options.forceDelete})
-      .then(() => swPrecache.generate(workerOptions))
+      .then(() => swPrecache.generate(this.workerOptions))
       .then((serviceWorkerFileContents) => {
         if (this.options.minify) {
           const uglifyFiles = {};
